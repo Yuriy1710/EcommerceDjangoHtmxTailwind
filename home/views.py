98cfb6bf8cde4e303b76231a391b4b7eeaadea19
Yurii Holovchenko <88655907+Yuriy1710@users.noreply.github.com>
2023-07-29 21:47:14 +0200

    Add files via upload

---
+++ home/views.py
@@ -0,0 +1,29 @@
+from django.db.models import Q
+from django.shortcuts import render
+from product.models import *
+
+
+def frontpage(request):
+    products = Product.objects.all()[0:8]
+    context = {'products': products}
+    return render(request, 'frontpage.html', context)
+
+
+def shop(request):
+    products = Product.objects.all()
+    categories = Category.objects.all()
+    
+    active_category = request.GET.get('category', '')
+    if active_category:
+        products = products.filter(category__slug=active_category)
+        
+    query = request.GET.get('query', '')
+    if query:
+        products = products.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    
+    context = {
+        'products': products, 
+        'categories': categories,
+        'active_category': active_category
+        }
+    return render(request, 'shop.html', context)
---
+++ product/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'Categories'
+        
+    def __str__(self):
+        return self.name
+      
+      
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created_at']
+        
+    def __str__(self):
+        return self.name   
